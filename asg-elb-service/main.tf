fb68cebd6bf351dd1cb84fcf9cd66e41358f6b92
vs-dexter <147118848+vs-dexter@users.noreply.github.com>
2023-11-23 14:57:40 +0800

    fix: typo

--- asg-elb-service/main.tf
+++ asg-elb-service/main.tf
@@ -89,7 +89,7 @@ data "aws_ami" "ubuntu" {
   filter {
     name   = "name"
     # values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
-    value = ["ubuntu/images/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["ubuntu/images/ubuntu-focal-20.04-amd64-server-*"]
   }
 }
 
